d7f488cabf46bcbdd0d019d199a81151ba806c4e
thdmn <kborodin1@gmail.com>
2021-06-11 17:14:05 +0300

    dynamic array

--- ds/Arrays/dynamic_arrays.py
+++ ds/Arrays/dynamic_arrays.py
@@ -6,29 +6,27 @@
 
 def dynamic_array(n, queries):
 
-	answers = [[] for i in range(n)]
-	sup = []
+	arr = [[] for i in range(n)]
+	answers = []
 	lastAnswer = 0
 
 	for element in queries:
 
 		if element[0] == 1:
 			idx = ((element[1] ^ lastAnswer) % n)
-			answers[idx].append(element[2])
-			print(answers[idx])
+			arr[idx].append(element[2])
+			print(arr[idx])
 			
 		elif element[0] == 2:
 			idx = ((element[1] ^ lastAnswer) % n)
-			lastAnswer = answers[idx][element[2] % len(answers[idx])]
-			sup.append(lastAnswer)
+			lastAnswer = arr[idx][element[2] % len(arr[idx])]
+			answers.append(lastAnswer)
 			
-			
-
-	print(sup)
-	return 'Aspergo'
+	return answers
 
 if __name__ == "__main__":
 	
 	num = 2
 	queries = [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]]
 	result = dynamic_array(num, queries)
+	print(result)
