ec21011d310e76bc48325aba7718cda22ad0d770
dmnth <kborodin1@gmail.com>
2021-07-06 21:26:36 +0300

    top view pass

--- ds/Bst/my_bst.py
+++ ds/Bst/my_bst.py
@@ -56,7 +56,14 @@ class binaryTree:
 
         current = self.root
         stack = []
+        level = 0
+        hash_map = {level: self.root}
+        """
+        
+            Traverse left child, if none --> switch to right child
+            Proceed until stack is empty
 
+        """
         while True:
 
             if current is not None:
@@ -65,13 +72,45 @@ class binaryTree:
 
             elif stack:
                 current = stack.pop()
-                print(current.value)
+                print(f"{current.value} at level {level}")
                 current = current.right 
 
             else:
                 break
 
-                
+    def vertical_order_traversal(self):
+
+        x_axis = 0
+        kiu = [(x_axis, self.root)]
+        hash_mape = {0: [self.root.value]}
+
+        while kiu:
+
+            x_distance, element = kiu.pop(0)
+
+            if element.left:
+
+                if x_distance-1 not in hash_mape.keys():
+                    hash_mape[x_distance - 1] = [element.left.value]
+                else:
+                    pass
+                    #hash_mape[x_distance - 1].append(element.left.value)
+
+                kiu.append((x_distance-1, element.left))
+
+            if element.right:
+
+                if x_distance+1 not in hash_mape.keys():
+                    hash_mape[x_distance + 1] = [element.right.value]
+                else:
+                    pass
+                    #hash_mape[x_distance + 1].append(element.right.value)
+
+                kiu.append((x_distance+1, element.right))
+
+        print(hash_mape)
+
+
         
 if __name__ == "__main__":
     
@@ -87,5 +126,6 @@ if __name__ == "__main__":
     tree.add_child(node_3)
     tree.add_child(node_4)
     tree.add_child(node_5)
-    obj = tree.root.get_right_child().get_right_child()
-    print(obj.get_left_child().get_value())
+
+    tree.traverseTree()
+    tree.vertical_order_traversal()
--- ds/Bst/tree_top_view.py
+++ ds/Bst/tree_top_view.py
@@ -15,6 +15,9 @@ def top_view(root_node):
 	hash_map = {}
 	level = 0
 
+	# Iterative in-order traversal:
+	# Way to count nodes on each level
+
 	while True:
 
 		if current_node is not None:
@@ -23,14 +26,13 @@ def top_view(root_node):
 
 		elif stack:
 			current_node = stack.pop()
-			hash_map[level+1] = current_node
 			print(current_node.info, end=" ")
 			current_node = current_node.right
 
 		else:
 			break
 
-	print(hash_map)
+
 	
 if __name__ == "__main__":
 	
@@ -39,8 +41,9 @@ if __name__ == "__main__":
 	board_elements = [39, 28, 51, 13, 37, 46, 60, 12, 14, 53, 65, 52]
 	test_string = "37 23 108 59 86 64 94 14 105 17 111 65 55 31 79 97 78 25 50 22 66 46 104 98 81 90 68 40 103 77 74 18 69 82 41 4 48 83 67 6 2 95 54 100 99 84 34 88 27 72 32 62 9 56 109 115 33 15 91 29 85 114 112 20 26 30 93 96 87 42 38 60 7 73 35 12 10 57 80 13 52 44 16 70 8 39 107 106 63 24 92 45 75 116 5 61 49 101 71 11 53 43 102 110 1 58 36 28 76 47 113 21 89 51 19 3"
 	#test_array = test_string.split()
-	test_array = [37, 23, 108, 59, 55, 86, 64, 94, 65, 14, 105, 17, 111, 115]
+	#test_array = [37, 23, 108, 59, 55, 86, 64, 94, 65, 14, 105, 17, 111, 115]
 	#test_array = [99, 60, 155, 41, 73, 81, 85, 86, 87, 80, 79, 78, 77, 40]
+	test_array = [1, 2, 5, 3, 6, 4]
 	for element in test_array:
 		my_tree.create(element)
 
