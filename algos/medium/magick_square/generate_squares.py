346e00679a29279d1385bef34780d5014024a690
dmnth <kborodin1@gmail.com>
2021-09-19 17:22:57 +0300

    did some stuff

--- algos/medium/magick_square/generate_squares.py
+++ algos/medium/magick_square/generate_squares.py
@@ -3,10 +3,14 @@ m = [[2, 9, 4],
 	[7, 5, 3], 
 	[6, 1, 8]]
 
-m = [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
-new_matrix = [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
+n = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
 
-for el in new_matrix:
+m = [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
+
+for el in n:
+	print(el)
+	
+for el in m:
 	print(el)
 
 
--- algos/medium/magick_square/rotate_matricies.py
+++ algos/medium/magick_square/rotate_matricies.py
@@ -4,17 +4,29 @@ def rotateClockwise(mtrx):
 	new_matrix = [[0,0,0], [0,0,0], [0, 0,0]]
 	for i in range(len(mtrx)):
 		for j in range(len(mtrx[0])-1,-1,-1):
-			new_matrix[i][j] = mtrx[j-1][i]
+			new_matrix[i][j] = mtrx[j][i]
+		
 	
 	return new_matrix
 
 def rotateCounterClockwise(mtrx):
 	return
 
+def rotate_matrix(m):
+	return [[m[j][i] for j in range(len(m))] for i in range(len(m[0])-1,-1,-1)]
+
+
 if __name__ == "__main__":
 
 	mtrx = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-	new_matrix = rotateClockwise(mtrx)
-	for el in new_matrix:
+	for el in mtrx:
 		print(el)
+	print('############')
+	new_matrix = rotateClockwise(mtrx)
+	for i in range(5):
+		
+		new_matrix = rotateClockwise(new_matrix)
+		print("##############")
+		for el in new_matrix:
+			print(el)
+
